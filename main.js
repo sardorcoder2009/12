a9668986557cac6049961928c2ce07ad13118b36
sardorn1coder <sardoriskandarov248@gmail.com>
2025-01-06 17:46:06 +0500

    lopo

--- main.js
+++ main.js
@@ -1,48 +1,34 @@
-const restaurants = [
-    { name: 'Azizbek', price: 25, address: 'Pyatiy' },
-    { name: 'Best', price: 30, address: 'Jorabayeva' },
-    { name: 'Lola', price: 4, address: 'Yubileyniy' },
-    { name: 'Uchrashuv', price: 20, address: 'Port' },
-    { name: 'Dasturxon', price: 22, address: 'Pyatiy' },
-    { name: 'Baliq', price: 20, address: 'Uchqizil' },
-    { name: 'Afrodita', price: 30, address: 'Yubileyniy' },
-    { name: 'Oxus', price: 40, address: 'Uchqizil' },
-    { name: 'Nur', price: 15, address: 'Jorabayeva' },
-    { name: 'Manti', price: 10, address: 'Devyatka' },
-    { name: 'Burger', price: 20, address: 'Yubileyniy' },
-    { name: 'Somsa', price: 10, address: 'Zelyoniy' },
-    { name: 'Tandir', price: 35, address: 'Jorabayeva' },
-    { name: 'Uzmir', price: 10, address: '75' },
-    { name: 'American', price: 20, address: 'Yubileyniy' },
-];
+let language = 'uz';
+let currentImage = 0;
 
-const form = document.querySelector('.form');
-const asos = document.querySelector('.asos');
-const addressInput = document.querySelector('#addressInput');
-
-function displayRestaurants(restaurantsList) {
-    const mappedRestaurants = restaurantsList.map(restaurant => {
-        return `<div class="katta">
-            <h1 class="name">${restaurant.name}</h1>
-            <p class="price">${restaurant.price} USD</p>
-            <p class="address">${restaurant.address}</p>
-        </div>`;
-    });
-    asos.innerHTML = mappedRestaurants.join('');
+function changeBackground() {
+  const body = document.body;
+  body.style.backgroundColor = body.style.backgroundColor === 'white' ? 'black' : 'white';
+  document.getElementById('logo').style.color = body.style.backgroundColor === 'white' ? 'black' : 'white';
 }
 
+function changeLanguage() {
+  if (language === 'uz') {
+    document.getElementById('welcome').textContent = 'Добро пожаловать!';
+    document.getElementById('extra').textContent = 'Больше ничего';
+    language = 'ru';
+  } else {
+    document.getElementById('welcome').textContent = 'Hush Kelibsiz!';
+    document.getElementById('extra').textContent = 'Ortiqcha hech narsa';
+    language = 'uz';
+  }
+}
 
-displayRestaurants(restaurants);
-JG 
-form.addEventListener('submit', function(event) {
-    
-    event.preventDefault();
-    
-    const filterText = addressInput.value.toLowerCase();
-    
-    const filteredRestaurants = restaurants.filter(restaurant => 
-        restaurant.address.toLowerCase().includes(filterText)
-    );
-    
-    displayRestaurants(filteredRestaurants);
-});
+setInterval(() => {
+  const images = document.querySelectorAll('.images .image-container img');
+  
+
+  images.forEach((image) => {
+    image.style.display = 'none';
+  });
+
+  images[currentImage].style.display = 'block';
+
+ 
+  currentImage = (currentImage + 1) % images.length;
+}, 3000);
